689eff332ff54781cd9ccb956aa43ab9dbe6f386
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-03-22 02:12:36 -0300

    Sidebar ChatList 1/2

---
+++ src/components/ChatListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './ChatListItem.css'
+
+export default () => {
+    return (
+        <div className="chatListItem">
+            <img className="chatListItem--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt=""  />
+            <div className="chatListItem--lines">
+                <div className="chatListItem--line" >
+                    <div className="chatListItem--name">Carlos Pinheiro</div>
+                    <div className="chatListItem--date">19:00</div>
+                </div>
+                <div className="chatListItem--line" >
+                <div className="chatListItem--lastMsg">
+                    <p>Hi, turo ben?</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+    }
