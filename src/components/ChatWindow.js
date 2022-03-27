043075dc37e9e3cbf413e6f779e0f4e57ecebc7a
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-03-27 01:53:50 -0300

    Chat Window Body

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -4,6 +4,11 @@ import './ChatWindow.css'
 import SearchIcon from '@material-ui/icons/Search'
 import AttachFileIcon from '@material-ui/icons/AttachFile'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import CloseIcon from '@material-ui/icons/Close'
+import SendIcon from '@material-ui/icons/Send'
+import MicIcon from '@material-ui/icons/Mic'
+
 
 
 export default () => {
@@ -29,8 +34,33 @@ export default () => {
                 </div>
 
             </div>
-            <div className="chatWindow--body"></div>
-            <div className="chatWindow--footer"></div>
+            <div className="chatWindow--body">
+
+            </div>
+            <div className="chatWindow--footer">
+                
+                <div className="chatWindow--pre">
+
+                <div className="chatWindow--btn">
+                        <InsertEmoticonIcon style={{color: '#919191'}} />
+                    </div>
+
+                </div>
+                <div className="chatWindow--inputarea">
+                    <input 
+                    className="chatWindow--input" 
+                    type="text"
+                    placeholder="Digite uma mensagem"
+                     />
+                </div>
+                <div className="chatWindow--pos">
+
+                <div className="chatWindow--btn">
+                        <SendIcon style={{color: '#919191'}} />
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
