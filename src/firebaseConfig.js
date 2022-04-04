1b5b62c0ecd18bc77fce5e12e9e961e7f7004253
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-04-04 03:07:51 -0300

    Configurando firebase no projeto

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDreuOJVImYx04fFmHClPzm6FU21IoWFnI",
+    authDomain: "whatsappclone-16b04.firebaseapp.com",
+    projectId: "whatsappclone-16b04",
+    storageBucket: "whatsappclone-16b04.appspot.com",
+    messagingSenderId: "371234856237",
+    appId: "1:371234856237:web:45074d2512f85fd84787a4"
+  };
