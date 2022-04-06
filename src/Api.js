6179d926f4acc90abcdd69e3c6610a6528e04b8f
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-04-05 23:45:43 -0300

    Login com Facebook

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,4 @@
+
 import firebase from 'firebase/app'
 import 'firebase/firebase-auth'
 import 'firebase/firebase-firestore'
@@ -7,3 +8,11 @@ import firebaseConfig from './firebaseConfig'
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
+
+export default {
+    fbPopup: async () => {
+        const provider = new firebase.auth.FacebookAuthProvider();
+        let result = await firebaseApp.auth().signInWithPopup(provider);
+        return result;
+    }
+};
